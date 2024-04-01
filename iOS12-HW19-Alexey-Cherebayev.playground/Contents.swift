083da4b1cca5eb14d48c6ff56cbe11c240df9ac2
AlexChe360 <devalex360@gmail.com>
2024-04-01 13:32:36 +0500

    Compleate task with star

--- iOS12-HW19-Alexey-Cherebayev.playground/Contents.swift
+++ iOS12-HW19-Alexey-Cherebayev.playground/Contents.swift
@@ -1,6 +1,7 @@
 import Foundation
+import CryptoKit
 
-func getData(urlRequest: String) {
+private func getData(urlRequest: String) {
     let urlRequest = URL(string: urlRequest)
     guard let url = urlRequest else { return }
     URLSession.shared.dataTask(with: url) { data, response, error in
@@ -23,4 +24,20 @@ func getData(urlRequest: String) {
     }.resume()
 }
 
-getData(urlRequest: "https://cat-fact.herokuapp.com/facts")
+func MD5(string: String) -> String {
+    let digest = Insecure.MD5.hash(data: Data(string.utf8))
+
+    return digest.map {
+        String(format: "%02hhx", $0)
+    }.joined()
+}
+
+// getData(urlRequest: "https://cat-fact.herokuapp.com/facts")
+
+let publicKey = "2c364e5311aa169780edc7732e67b079"
+let privateKey = "8461de92f59c753f5d5e17d897f4b1dc6a38c32f"
+let hash = MD5(string: String(1) + privateKey + publicKey)
+let apiUrl = "https://gateway.marvel.com/v1/public/comics/34?ts=1&apikey=\(publicKey)&hash=\(hash)"
+getData(urlRequest: apiUrl)
+
+
